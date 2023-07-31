368048c1d0b8b4da77b4d50c38ae6d30f5d837be
Hessi <108060787+h30h30@users.noreply.github.com>
2023-08-01 00:33:57 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xb3bf34b6fc2f1bae365e5aee78c668ba88bd5ac0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
